77f566e50a66c8d77a3ca48bbf74e76686c711e0
amanfoundongithub <99312253+amanfoundongithub@users.noreply.github.com>
2022-04-17 12:50:15 +0530

    Added Q3d.sh

--- Q3d.sh
+++ Q3d.sh
@@ -2,9 +2,12 @@
 
 IFS=$'\n'       # make newlines the only separator
 set -f 
-touch temp.txt         # disable globbing
+touch temp.txt
+kp=1         # disable globbing
 for i in $(cat < "$1"); do
   lin=$i
   echo $i > temp.txt
+  echo -n "$kp-"
+  kp=$((kp+1))
   wc -w < temp.txt
 done
