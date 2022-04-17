9bbfb10998aca1492e271529a571e1a502680b35
amanfoundongithub <99312253+amanfoundongithub@users.noreply.github.com>
2022-04-17 12:49:35 +0530

    Added Q3e.sh

--- Q3e.sh
+++ Q3e.sh
@@ -4,7 +4,7 @@ grep -wo '[[:alnum:]]\+' $1  | sort | uniq -d > temp.txt
 
 cat temp.txt | while read line 
 do
-   echo -n "$line:"
+   echo -n "$line-"
    grep -o "$line" $1 | wc -l   
-   # do something with $line here
+
 done
